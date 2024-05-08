a5a3ea9dc528f4589c3bfd1983f899a400ee6453
Rahulns21 <rahul9380767545@gmail.com>
2024-05-08 22:24:25 +0530

    forgot password

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('forgot-password/', views.forgot_password, name='forgot-password'),
+    path('reset-password/<uidb64>/<token>/', views.reset_password_validate, name='reset-password-validate'),
+    path('reset-password/', views.reset_password, name='reset-password'),
 ]
